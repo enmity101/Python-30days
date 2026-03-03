6e1f4e9cdd42392671fa6d8a31256384f78d6feb
Harsh Singh <161110319+enmity101@users.noreply.github.com>
2026-03-03 23:51:22 +0530

    Add files via upload

---
+++ Py-Code/name.py
@@ -0,0 +1,20 @@
+print('My Name is Harsh')
+print('Harsh wants to become python expert')
+print('Harsh is Learning python')
+
+name = 'Parth'
+print('My name is ', name)
+print(name ,' wants to become python expert')
+print(name ,' is Learning python')
+
+name = 'Harsh'
+print('My name is ', name)
+print(name, ' wants to become python expert')  
+print(name, ' is Learning python')
+
+name = 'Harsh'
+language = 'Python'
+print('My name is ', name)
+print(name, 'wants to become ', language, ' expert')  
+print(name, 'is Learning', language)
+
