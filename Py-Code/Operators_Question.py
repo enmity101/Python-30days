9e2ddb03cabcd53ed2c86e808c02139ce0c399a8
Harsh Singh <161110319+enmity101@users.noreply.github.com>
2026-03-09 00:08:40 +0530

    Add files via upload

---
+++ Py-Code/Operators_Question.py
@@ -0,0 +1,107 @@
+# ============================================
+#      BEGINNER PRACTICE QUESTIONS (PYTHON)
+# ============================================
+
+# Solve these 5 questions on your own.
+
+# Q1
+# If a = 15 and b = 4, what will be the output of:
+# print(a + b)
+# print(a - b)
+a = 15
+b = 4
+print(a + b) # output: 19
+print(a - b) # output: 11
+
+
+# Q2
+# x = 10
+# x += 7
+# x -= 2
+# What is the final value of x?
+
+x = 10
+x += 7
+x -= 2
+print(x) # output: 15
+
+# Q3
+# What will be the result of:
+# print(20 > 15)
+# print(10 == 5)
+
+print(20 > 15) # output: True
+print(10 == 5) # output: False
+
+# Q4
+# is_student = True
+# has_id = False
+# What will be the output of:
+# print(is_student and has_id)
+# print(is_student or has_id)
+
+is_student = True
+has_id = False
+print(is_student and has_id) # output: False
+print(is_student or has_id) # output: True
+
+# Q5
+# names = ["Harsh", "Ravi", "Priya"]
+# What will these return?
+# print("Ravi" in names)
+# print("Aman" not in names)
+
+names = ["Harsh", "Ravi", "Priya"]
+print("Ravi" in names) # output: True
+print("Aman" not in names) # output: True
+
+# Q6
+# If m = 9 and n = 2, what will be the output of:
+# print(m * n)
+# print(m ** n)
+
+m = 9
+n = 2
+print(m * n)  # output: 18
+print(m ** n) # output: 81
+
+# Q7
+# y = 25
+# y //= 4
+# y %= 3
+# What is the final value of y?
+
+y = 25
+y //= 4
+y %= 3
+print(y) # output: 0
+
+# Q8
+# What will be the result of:
+# print(7 != 7)
+# print(12 <= 20)
+
+print(7 != 7)   # output: False
+print(12 <= 20) # output: True
+
+# Q9
+# passed_exam = True
+# submitted_project = True
+# What will be the output of:
+# print(passed_exam and submitted_project)
+# print(not submitted_project)
+
+passed_exam = True
+submitted_project = True
+print(passed_exam and submitted_project) # output: True
+print(not submitted_project)             # output: False
+
+# Q10
+# nums = [5, 10, 15, 20]
+# What will these return?
+# print(10 in nums)
+# print(25 in nums)
+
+nums = [5, 10, 15, 20]
+print(10 in nums) # output: True
+print(25 in nums) # output: False
