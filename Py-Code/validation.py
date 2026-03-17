594d0da5215514af6213f079177a8bf14b49ba5c
Harsh Singh <161110319+enmity101@users.noreply.github.com>
2026-03-18 03:32:00 +0530

    Add files via upload

---
+++ Py-Code/validation.py
@@ -0,0 +1,13 @@
+# Validation of data
+country = "INDIA"
+print(country.isalpha())
+
+# Check if the string contains only digits
+phone = "+91-89546-74545"
+print(phone.isnumeric())
+
+# Remove non-numeric characters from the phone number
+phone1 = "+91-89546-74545"
+phone1 = "8954674545"
+print(phone1.isnumeric())
+
